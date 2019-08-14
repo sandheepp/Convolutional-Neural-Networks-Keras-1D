f93413992659a0f25ed68b69226f33bfcfcec917
sandheepp <sandheep.p01gmail.com>
2019-08-14 14:03:53 +0530

    added prediction

--- network.py
+++ network.py
@@ -1,37 +1,59 @@
+import time
+t = time.time()
+
 #Import
 from dataset import data, label
+
+# do stuff
+elapsed = time.time() - t
+
+print("Time taken for data import(in sec):"+str(elapsed))
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Embedding
 from keras.layers import Conv1D, GlobalAveragePooling1D, MaxPooling1D
-from keras import optimizers
+from keras import optimizers, metrics
 from keras import backend as K
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 import numpy as np
 
+#input data shape
+data_shape=data.shape[1] 
+
 #CNN network
 model = Sequential()
-model.add(Conv1D(filters= 32, kernel_size = 5, input_shape=(700,1)))
+model.add(Conv1D(filters= 32, kernel_size = 5, input_shape=(data_shape,1)))
 model.add(MaxPooling1D(pool_size=2,strides=2))
 model.add(Dropout(0.1))
 model.add(Flatten())
 model.add(Dense(1024, activation='sigmoid'))
 model.add(Dense(32, activation='sigmoid'))
+model.add(Dense(2, activation='sigmoid'))
 model.add(Dense(1, activation='sigmoid'))
-model.add(Activation('softmax'))
+#model.add(Activation('softmax'))
 
 #Optimizers
 sgd = optimizers.SGD(lr=0.005, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='mean_squared_error', optimizer=sgd)
 
-#Data Splitting
+#Configures the model for training.
+model.compile(loss='mean_squared_error', optimizer=sgd, metrics=['accuracy'])
+
+#Data Splitting: Test, train
 X_train, X_test, y_train, y_test = train_test_split(data, label, test_size=0.1, random_state=0)
 
-#training
-model.fit(X_train, y_train, batch_size=10, epochs=1)
+# Train to train, CV
+X_train, X_CV, y_train, y_CV = train_test_split(X_train, y_train, test_size=0.1, random_state=0)
 
-#evaluate
-score = model.evaluate(X_test, y_test, batch_size=10)
+#Training : Trains the model for a given number of epochs (iterations on a dataset).
+model.fit(X_train, y_train, batch_size=64, epochs=1)
+
+#evaluate: Returns the loss value & metrics values(accuracy) for the model in test mode.
+score = model.evaluate(X_CV, y_CV, batch_size=10)
 print(score)
+
+#Generates output predictions for the input samples.
+#predictions = model.predict(X_test, batch_size=None, verbose=0)
+#print(predictions)
+
